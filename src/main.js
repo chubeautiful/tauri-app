1811b8487c5002ff2023e6ed4af11627ab3e8992
chubeautiful <565499216@qq.com>
2024-12-27 19:29:06 +0800

    11

---
+++ src/main.js
@@ -0,0 +1,30 @@
+const { invoke } = window.__TAURI__.core;
+
+let greetInputEl;
+let greetMsgEl;
+
+async function greet() {
+  // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
+}
+
+async function testBtn(){
+  alert("test")
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  greetInputEl = document.querySelector("#greet-input");
+  greetMsgEl = document.querySelector("#greet-msg");
+  document.querySelector("#greet-form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    greet();
+  });
+
+  document.querySelector("#greet-form").addEventListener("button", (e) => {
+    e.preventDefault();
+    console.log("test btn")
+    testBtn();
+  });
+});
+
+
